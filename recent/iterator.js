2ccfab241419ca63a6a289f16b8d0e4d7f8f7078
dongzhe <dongzhe@360.cn>
2016-03-04 10:28:44 +0800

    update code

--- recent/iterator.js
+++ recent/iterator.js
@@ -13,7 +13,7 @@ var Iterator = function(obj) {
     current = current + 1;
   }
   var isDone = function() {
-    return current > = obj.length;
+    return (current >= obj.length);
   }
   var getCurrentItem = function() {
     return obj[current];
@@ -24,3 +24,34 @@ var Iterator = function(obj) {
     getCurrentItem: getCurrentItem
   }
 }
+
+var compare = function(iterator1, iterator2) {
+  while (!iterator1.isDone() && !iterator2.isDone()) {
+    if (iterator1.getCurrentItem() != iterator2.getCurrentItem()) {
+      return false;
+    }
+    iterator1.next();
+    iterator2.next();
+  }
+  return true;
+}
+
+var iterator1 = Iterator([1, 2, 3])
+var iterator2 = Iterator([1, 2, 3])
+
+compare(iterator1, iterator2);
+
+var stopeach = function(obj, callback) {
+  for (var i = 0, l, l = obj.length; i < l; i++) {
+    if (callback(i, obj[i]) === false) {
+      break;
+    }
+  }
+}
+
+stopeach([1, 2, 3, 4, 5], function(index, ele) {
+  if (index > 3) {
+    return false;
+  }
+  console.log(index, ele);
+})
